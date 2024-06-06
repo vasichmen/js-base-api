3626be0b30579d457191315875cfbaf7688e2db7
Василий Метт <mettvd@sibintek.ru>
2024-06-06 21:09:36 +0300

    Rename package

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,19 +1,14 @@
 import buble from '@rollup/plugin-buble'; // Транспиляция/добавление полифилов для умеренной поддержки браузеров
 import commonjs from '@rollup/plugin-commonjs'; // Конвертирование CommonJS модулей в ES6
-import vue from 'rollup-plugin-vue'; // Обработка однофайловых компонентов .vue
 
 export default {
-  input: 'src/wrapper.js', // Путь до относительного package.json
+  input: 'src/index.js',
   output: {
-    name: 'VueHeatmap',
+    name: 'JsBaseApi',
     exports: 'named',
   },
   plugins: [
     commonjs(),
-    vue({
-      css: true, // Динамически внедряем CSS в тег <style>
-      compileTemplate: true, // Явное преобразование шаблона в рендер-функцию
-    }),
     buble({
       objectAssign: 'Object.assign',
     }), // Транспиляция в ES5
